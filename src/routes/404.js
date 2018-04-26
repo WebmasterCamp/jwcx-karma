4b58ab3f30ac5a012a0dc73428ad30b944316833
Pmc Dev <phoomparin@gmail.com>
2018-04-27 04:27:19 +0700

    :wrench: Added Spin Loader in place of 404

--- src/routes/404.js
+++ src/routes/404.js
@@ -1,3 +1,43 @@
 import React from 'react'
+import styled from 'react-emotion'
+import {Spin} from 'antd'
 
-export default () => <h1>404 Not Found</h1>
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  min-width: 100vw;
+  min-height: 100vh;
+
+  background-image: linear-gradient(rgb(105, 115, 173), rgb(107, 201, 233));
+  background-attachment: fixed;
+
+  padding: 0 2em;
+`
+
+const Paper = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+
+  margin-bottom: 3.2em;
+  box-shadow: rgba(0, 0, 0, 0.18) 0px 3px 18.5px 2px;
+
+  width: 100%;
+  padding: 1.8em 2.2em;
+  border-radius: 8px;
+  background: rgba(255, 255, 255, 0.94);
+`
+
+const NotFound = () => (
+  <Container>
+    <Paper>
+      <Spin />
+    </Paper>
+  </Container>
+)
+
+export default NotFound
