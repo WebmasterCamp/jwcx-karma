09238974bd1241e541b6e6d7a3074b530cfba245
chunza2542 <chunza2542@hotmail.com>
2018-04-26 18:43:12 +0700

    :fire: WIP: Cleanup

--- src/ducks/root.js
+++ src/ducks/root.js
@@ -4,9 +4,6 @@ import {persistCombineReducers} from 'redux-persist'
 import {reducer as form} from 'redux-form'
 
 import user, {reauthSaga, userWatcherSaga} from './user'
-import camper, {camperWatcherSaga} from './campers'
-import grading, {gradingWatcherSaga} from './grading'
-import filter, {filterWatcherSaga} from './filter'
 
 const config = {
   key: 'root',
@@ -17,18 +14,12 @@ const config = {
 
 export const reducers = persistCombineReducers(config, {
   user,
-  camper,
-  filter,
   form,
-  grading,
 })
 
 export function* rootSaga() {
   yield all([
     reauthSaga(),
     userWatcherSaga(),
-    camperWatcherSaga(),
-    gradingWatcherSaga(),
-    filterWatcherSaga(),
   ])
 }
